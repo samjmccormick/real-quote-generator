0f58026f129128e8b7ac1848b68bb6a1fcaaac81
samjmccormick <samjmccormick@gmail.com>
2024-10-03 12:34:36 -0700

    added react-bootstrap can't get anything to work, really struggling with react, going to write in js to feel good about myself

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
 
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let quoted = '';
+
+  async function GetQuote() {
+    try {
+        const res = await fetch("https://api.kanye.rest/");
+        const data = await res.json();
+        console.log(data.quote);
+        quoted = data.quote;
+    } catch (err) {
+        console.log(err);
+    }
+  };
+
+  GetQuote();
+
+console.log(quoted);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>Quote</div>
+   <Button variant='primary'>Quote</Button>
     </>
   )
 };
